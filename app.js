f13569821dba821ed548dabade6b8f2d444e94a5
Vigan Ramadani <vigancseubt@hotmail.com>
2021-03-08 22:07:46 +0100

    setting up middlewares and adding node modules

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
 const express = require('express');
+const app = express();
+
+const path = require('path');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+
+dotenv.config({path : './config.env'});
+
+mongoose.connect(process.env.DATABASE_LOCAL, {
+    useNewUrlParser : true,
+    useUnifiedTopology : true,
+    useCreateIndex : true
+});
+
+//middleware flash messages
+app.use(flash());
+
+//setting middlware globally
+app.use((req, res, next)=> {
+    res.locals.success_msg = req.flash(('success_msg'));
+    res.locals.error_msg = req.flash(('error_msg'));
+    res.locals.error = req.flash(('error'));
+    res.locals.currentUser = req.user;
+    next();
+});
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.use(userRoutes);
+
+app.listen(process.env.PORT, ()=> {
+    console.log('Server is started');
+});
